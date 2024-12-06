260b79b3216c548edeb8955ab43d96f77e8a043b
kamra-n <ikamihere@gmail.com>
2024-12-06 13:13:43 +0500

    how to create dynamic list and data type specified list

--- List/list.dart
+++ List/list.dart
@@ -1,1 +1,22 @@
-void main() {}
+void main() {
+  // Dynamic list kese bnayein jo har tarha ki data type accept kre.
+
+  List dynamicTypeList = [
+    'Kamran',
+    'Bilal',
+    'Ali',
+    1,
+    2,
+    3,
+    true,
+    30.6,
+    {'name': 'Kamran'}
+  ];
+  print(dynamicTypeList);
+
+  //data type specific list kese bnayein agar is mein ap data type string rkhte ho or value int ki yeh kisi or data type ki dete ho tou apko error aega.
+  // yha mene data type specific list bnayi hai jo just string data type accept kre gyi.
+
+  List<String> dataTypeSpecifiedList = ['Kamran,Bilal', "Ali"];
+  print(dataTypeSpecifiedList);
+}
