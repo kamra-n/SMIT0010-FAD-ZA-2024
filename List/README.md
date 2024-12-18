963c07b1e9b7506070611bad3e46f2eec1b53b75
kamra-n <ikamihere@gmail.com>
2024-12-18 21:48:59 +0500

    updating list methods

---
+++ List/README.md
@@ -0,0 +1,41 @@
+# List and Methods in dart
+
+How to create dynamic list in dart
+
+```dart
+ List dynamicTypeList = [
+    'Kamran',
+    'Bilal',
+    'Ali',
+    1,
+    2,
+    3,
+    true,
+    30.6,
+    {'name': 'Kamran'}
+  ];
+  print(dynamicTypeList);
+```
+
+How to create data type specific list in dart
+
+```dart
+List<String> dataTypeSpecifiedList = ['Kamran,Bilal', "Ali"];
+ print(dataTypeSpecifiedList);
+```
+
+```dart
+ var lst = [1, 2, 3, 4, 5];
+  var e1 = lst.first;
+  print(e1);
+```
+
+-- this method provide first element from list
+
+```dart
+var lst = [1, 2, 3, 4, 5];
+var e1 = lst.last;
+print(e1);
+```
+
+-- this method provide last element from list
--- List/list.dart
+++ List/list.dart
@@ -1,22 +1,31 @@
 void main() {
   // Dynamic list kese bnayein jo har tarha ki data type accept kre.
 
-  List dynamicTypeList = [
-    'Kamran',
-    'Bilal',
-    'Ali',
-    1,
-    2,
-    3,
-    true,
-    30.6,
-    {'name': 'Kamran'}
-  ];
-  print(dynamicTypeList);
+  // List dynamicTypeList = [
+  //   'Kamran',
+  //   'Bilal',
+  //   'Ali',
+  //   1,
+  //   2,
+  //   3,
+  //   true,
+  //   30.6,
+  //   {'name': 'Kamran'}
+  // ];
+  // print(dynamicTypeList);
 
   //data type specific list kese bnayein agar is mein ap data type string rkhte ho or value int ki yeh kisi or data type ki dete ho tou apko error aega.
   // yha mene data type specific list bnayi hai jo just string data type accept kre gyi.
 
-  List<String> dataTypeSpecifiedList = ['Kamran,Bilal', "Ali"];
-  print(dataTypeSpecifiedList);
+  // List<String> dataTypeSpecifiedList = ['Kamran,Bilal', "Ali"];
+  // print(dataTypeSpecifiedList);
+
+  // var e1 = lst.first;
+  // print(e1);
+  // this method provide first element from list
+  var lst = [1, 2, 3, 4, 5];
+  var e2 = lst.last;
+  
+  var e3 = lst.elementAt(1);
+  var len = lst.length;
 }
